f34ab9fb972eb6d0eeb01ef3a497a189fa5e0e87
Mister BRUNO <brunopaul1412@gmail.com>
2026-09-20 16:40:48 +0200

    Securisation des identifiants via un fichier .env

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Charge les variables du fichier .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 try {
     // mysql => indique le moteur de la BDD
     // host=localhost => l'adresse du serveur
     // dbname=studio_exemple => nom de la base de données
-    $dns = "mysql:host=gateway01.eu-central-1.prod.aws.tidbcloud.com;port=4000;database=prenom_pokedex_bdd;";
+    $dns = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};database={$_ENV['DB_NAME']};";
     // Utilisateur avec lequel se connecter a la BDD
-    $utilisateur = "35a8BVBwx695s1o.root";
-    $motDePasse = "xuCdeLspsruJnjZ8";
+    $utilisateur = $_ENV['DB_USER'];
+    $motDePasse = $_ENV['DB_PASSWORD'];
 
     $options = [
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
